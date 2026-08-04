4de2b969b6748bac4566156d6ef7da3ac7b81a3e
mainamuchiru <mainaephantus@gmail.com>
2026-08-04 11:08:52 +0300

    auth.py file created

---
+++ server/auth.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from flask import session, jsonify
+
+
+def login_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if "user_id" not in session:
+            return jsonify({"error": "Unauthorized"}), 401
+        return f(*args, **kwargs)
+    return wrapper
