edd8a1b61c58f24946956626041d6d4cf5f876d4
mainamuchiru <mainaephantus@gmail.com>
2026-08-04 11:12:45 +0300

    app.py file created

---
+++ server/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .config import db, bcrypt, migrate
+from .routes import register_routes
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "super-secret-key"
+
+db.init_app(app)
+bcrypt.init_app(app)
+migrate.init_app(app, db)
+
+register_routes(app)
