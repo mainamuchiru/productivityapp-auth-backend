dc2a6e844511d0c8ec1a3f25879b62fb8812aa3a
mainamuchiru <mainaephantus@gmail.com>
2026-08-04 11:39:47 +0300

    several files uploaded

--- server/auth.py
+++ server/auth.py
@@ -3,9 +3,13 @@ from flask import session, jsonify
 
 
 def login_required(f):
+
     @wraps(f)
     def wrapper(*args, **kwargs):
+
         if "user_id" not in session:
             return jsonify({"error": "Unauthorized"}), 401
+
         return f(*args, **kwargs)
+
     return wrapper
--- server/models.py
+++ server/models.py
@@ -1,46 +1,79 @@
 from sqlalchemy_serializer import SerializerMixin
-from config import db, bcrypt
+from .config import db, bcrypt
 
 
 class User(db.Model, SerializerMixin):
+
     __tablename__ = "users"
 
     serialize_rules = ("-_password_hash", "-notes.user")
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
 
-    notes = db.relationship("Note", back_populates="user", cascade="all, delete-orphan")
+    username = db.Column(
+        db.String,
+        unique=True,
+        nullable=False
+    )
+
+    _password_hash = db.Column(
+        db.String,
+        nullable=False
+    )
+
+    notes = db.relationship(
+        "Note",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
 
     def set_password(self, password):
-        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self._password_hash = bcrypt.generate_password_hash(
+            password
+        ).decode("utf-8")
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(self._password_hash, password)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
+        return bcrypt.check_password_hash(
+            self._password_hash,
+            password
+        )
 
 
 class Note(db.Model, SerializerMixin):
+
     __tablename__ = "notes"
 
     serialize_rules = ("-user.notes",)
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.Text, nullable=False)
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    title = db.Column(
+        db.String,
+        nullable=False
+    )
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
+    )
+
     updated_at = db.Column(
         db.DateTime,
         server_default=db.func.now(),
-        onupdate=db.func.now(),
+        onupdate=db.func.now()
     )
 
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    user = db.relationship("User", back_populates="notes")
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id")
+    )
 
-    def __repr__(self):
-        return f"<Note {self.title}>"
+    user = db.relationship(
+        "User",
+        back_populates="notes"
+    )
